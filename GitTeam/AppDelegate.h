3159fd70b5b17610a06c8bdc592c3f5c6a12e080
liwangwnag123 <2317790024@qq.com>
2017-04-27 13:45:08 +0800

    Initial Commit

---
+++ GitTeam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTeam
+//
+//  Created by 王力 on 2017/4/27.
+//  Copyright © 2017年 王力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitTeam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTeam
+//
+//  Created by 王力 on 2017/4/27.
+//  Copyright © 2017年 王力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
